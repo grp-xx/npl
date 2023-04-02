71b19fa005b56de7ca76495572c742e2a33a1ea8
Gregorio Procissi <gregorio.procissi@unipi.it>
2023-04-02 17:26:01 +0200

    fundamental members of socket class

--- lib/libnpl/socket.hpp
+++ lib/libnpl/socket.hpp
@@ -1,5 +1,80 @@
 #ifndef _SOCKET_HPP_
 #define _SOCKET_HPP_
+#include <cerrno>
+#include <cstdint>
+#include <system_error>
+#include <vector>
+#include <sys/socket.h>
+#include <unistd.h>
+
+namespace npl {
+
+typedef std::vector<uint8_t> buffer;
+
+template<int F, int type>
+class socket 
+{
+private:
+    int _sockfd;
+
+public:
+    explicit socket(int protocol = 0) 
+    {
+        if ( (_sockfd = ::socket(F, type, protocol) ) == -1 ) {
+            throw std::system_error(errno, std::generic_category(),"socket");
+        }
+    }
+
+    socket(const socket&) = delete;
+    socket& operator=(const socket&) = delete;
+
+    socket(socket&& rhs) 
+    :_sockfd(rhs._sockfd)
+    {
+        rhs._sockfd = -1;
+    }
+
+    socket& operator=(socket&& rhs)
+    {
+        if (*this != rhs)
+        {
+            this->close();
+            _sockfd = rhs._sockfd;
+            rhs._sockfd = -1;
+        }
+        return *this;
+    }
+
+    ~socket()
+    {
+        if (_sockfd != - 1) 
+        {
+            this->close();
+        }
+    }
+
+    void close()
+    {
+        if (_sockfd != -1) 
+        {
+            ::close(_sockfd);
+            _sockfd = -1;
+        }
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+}
+
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <sys/socket.h>
+#include "socket.hpp"
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+    npl::socket<AF_UNIX, SOCK_DGRAM> sock;
+
 }
