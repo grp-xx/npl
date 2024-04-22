16f09d8a006b0bd3f28af1427048c16724f4383e
“Gregorio <gregorio.procissi@unipi.it>
2024-04-22 15:50:14 +0200

    added extra I/O methods to socket

--- lib/libnpl/socket.hpp
+++ lib/libnpl/socket.hpp
@@ -103,16 +103,29 @@ public:
         return ::write(_sockfd, &buf[0], buf.size());
     }
 
-    std::ptrdiff_t read(buffer& buf) const
+    std::ptrdiff_t writen(const buffer& buf, ssize_t n)
     {
-        return ::read(_sockfd, &buf[0], buf.size());
+        std::ptrdiff_t num_written;
+        std::ptrdiff_t tot_written = 0;
+
+        while (tot_written < n ) {
+            num_written = ::write(_sockfd, &buf[tot_written], n-tot_written); 
+
+            if (num_written <= 0)  // An error has happened
+            {
+                if (num_written == -1 && errno == EINTR) 
+                    continue;     // Restart writing bythes form scratch in case of interrupt
+                else 
+                    return -1;  
+            }  
+            tot_written += num_written;
+        }
+        return tot_written;
     }
 
-    buffer read(size_t n) const 
+    std::ptrdiff_t      send(const buffer& buf, int flags = 0) const
     {
-        buffer buf(n);
-        int nbytes = ::read(_sockfd, &buf[0], buf.size());
-        return buffer(buf.begin(),buf.begin()+nbytes);
+       return ::send(_sockfd, &buf[0], buf.size(), flags);
     }
 
     std::ptrdiff_t 
@@ -120,6 +133,79 @@ public:
     {
         return ::sendto(_sockfd, &buf[0], buf.size(), flags, &remote.c_addr(), remote.len());
     }
+
+    std::ptrdiff_t read(buffer& buf) const
+    {
+        return ::read(_sockfd, &buf[0], buf.size());
+    }
+    buffer read(size_t n) const 
+    {
+        buffer buf(n);
+        int nbytes = ::read(_sockfd, &buf[0], buf.size());
+        return buffer(buf.begin(),buf.begin()+nbytes);
+    }
+
+    std::ptrdiff_t readn(buffer& buf, ssize_t n ) const
+    {
+        std::ptrdiff_t num_read;
+        std::ptrdiff_t tot_read = 0;
+
+        while(tot_read < n)
+        {
+            num_read = ::read(_sockfd, &buf[tot_read], n - tot_read);
+
+            if (num_read == 0)
+                return tot_read;  // EOF reached;
+
+            if (num_read == - 1)  // An error has happened
+            {
+                if (errno == EINTR) 
+                    continue;     // Restart reading bytes in case of interrupt
+                else 
+                    return -1;  
+            } 
+            tot_read+= num_read;
+
+        }
+        return tot_read;
+    }
+
+        buffer     readn(ssize_t len ) const
+    {
+        buffer buf(len);
+        auto tot_read = this->readn(buf,len);
+        return buffer(buf.begin(),buf.begin()+tot_read);
+    }
+
+
+    std::ptrdiff_t      recv(buffer& buf, int flags = 0) const
+    {
+        return ::recv(_sockfd, &buf[0], buf.size(), flags);
+    }
+
+    buffer              recv(int len, int flags = 0) const
+    {
+        buffer buf(len);
+        std::ptrdiff_t n = ::recv(_sockfd, &buf[0], buf.size(), flags);
+        return buffer(buf.begin(),buf.begin()+n);
+    }
+
+    std::ptrdiff_t      recvn(buffer& buf, int flags = 0) const
+    {
+        return ::recv(_sockfd, &buf[0], buf.size(), flags | MSG_WAITALL);
+    }
+
+    
+    buffer              recvn(int len, int flags = 0) const
+    {
+        buffer buf(len);
+        std::ptrdiff_t n = ::recv(_sockfd, &buf[0], buf.size(), flags | MSG_WAITALL);
+        return buffer(buf.begin(),buf.begin()+n);
+    }
+
+
+
+
 
     std::ptrdiff_t
     recvfrom(buffer& buf, sockaddress<F>& remote, int flags = 0) const
