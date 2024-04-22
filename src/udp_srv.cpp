0b977fcb955e04254171e26dd5dbc8a83434dbe8
“Gregorio <gregorio.procissi@unipi.it>
2024-04-22 12:46:52 +0200

    added UDP server

---
+++ src/udp_srv.cpp
@@ -0,0 +1,26 @@
+#include "sockaddress.hpp"
+#include "socket.hpp"
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <iostream>
+#include <sys/socket.h>
+
+int main()
+{
+    npl::socket<AF_INET, SOCK_DGRAM> sock;
+    // npl::sockaddress<AF_INET> srv_addr("localhost",15000);
+    npl::sockaddress<AF_INET> srv_addr(15000);
+    sock.bind(srv_addr);
+
+    for(;;)
+    {
+        auto [buf, clt] = sock.recvfrom(512);
+        std::cout << "Received request from IP: " << clt.host() << " - Port: " << clt.port() << std::endl;
+        std::transform(buf.begin(),buf.end(),buf.begin(),::toupper);
+        sock.sendto(buf, clt); 
+    }
+
+    sock.close();
+    return EXIT_SUCCESS;
+}
