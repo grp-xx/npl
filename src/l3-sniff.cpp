ecf7f0b8fd18a43d3d0ff1bc9cc27bec1956b604
Gregorio Procissi <gregorio.procissi@unipi.it>
2024-05-06 12:25:04 +0200

    added simple l3-sniffer

---
+++ src/l3-sniff.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include "socket.hpp"
+#include "sockaddress.hpp"
+
+int main()
+{
+    npl::socket<AF_INET, SOCK_RAW> sock(IPPROTO_ICMP);
+
+    for(;;)
+    {
+        auto [buf, from] = sock.recvfrom(1500);
+
+        if (buf.size() >= sizeof(struct ip)) {
+            const struct ip* iphdr = reinterpret_cast<const ip*>(&buf[0]);
+            npl::sockaddress<AF_INET> src(iphdr->ip_src);
+            npl::sockaddress<AF_INET> dst(iphdr->ip_dst);
+            std::cout << from.host() << " ---> " << dst.host() << std::endl;
+        }
+
+    }
+
+    return EXIT_SUCCESS;
+}
