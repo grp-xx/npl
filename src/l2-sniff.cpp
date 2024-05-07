278591934af7f1673dcdfbf657d20c5a332b1120
Gregorio Procissi <gregorio.procissi@unipi.it>
2024-05-07 12:54:10 +0200

    added simple l2 sniffer

---
+++ src/l2-sniff.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+#include <sys/socket.h>
+#include "headers.hpp"
+#include "sockaddress.hpp"
+#include "socket.hpp"
+#include <net/ethernet.h>
+
+
+int main()
+{
+    npl::socket<AF_PACKET, SOCK_DGRAM> sock(ntohs(ETHERTYPE_IP));
+    npl::sockaddress<AF_PACKET> device("eth0");
+    sock.bind(device);
+    sock.set_promisc("eth0");
+
+
+    for(;;)
+    {
+        auto buf = sock.recv(2000);
+        npl::header<hdr::ipv4> iph(buf);
+        std::cout << iph.src() << "  ---->  " << iph.dst()  << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
