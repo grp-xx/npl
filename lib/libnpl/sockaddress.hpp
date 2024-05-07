940f62b07e18119c1a0bea018ccbfdb9350c0fd6
Gregorio Procissi <gregorio.procissi@unipi.it>
2024-05-07 12:53:34 +0200

    added sockaddress for AF_PACKET

--- lib/libnpl/sockaddress.hpp
+++ lib/libnpl/sockaddress.hpp
@@ -4,12 +4,20 @@
 #include <string>
 #include <cstring>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <net/if.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <sys/un.h>
 #include <system_error>
 #include <utility>
+#include <sstream>
+
+#ifdef __linux__
+    #include <linux/if_packet.h>
+    #include <linux/if_ether.h>
+#endif
 
 
 namespace npl {
@@ -43,14 +51,12 @@ public:
         #endif
         strncpy(_addr.sun_path+offset,sockname.c_str(),sizeof(_addr.sun_path)-1);
     }
-
     
     sockaddress(const sockaddress&)            = default;
     sockaddress& operator=(const sockaddress&) = default;
     sockaddress(sockaddress&&)                 = default;
     sockaddress& operator=(sockaddress&&)      = default;
     ~sockaddress()                             = default;
-
 
     std::string 
     name() const
@@ -253,6 +259,135 @@ class sockaddress<AF_INET6> {
 
 };
 
+#ifdef __linux__
+
+    template<>
+    class sockaddress<AF_PACKET>
+    {
+    private:
+        socklen_t   _len;
+        sockaddr_ll _addr;
+
+    public:
+        sockaddress()
+        : _len(sizeof(sockaddr_ll))
+        {
+            memset(&_addr, 0, sizeof(sockaddr_ll));
+            _addr.sll_family = AF_PACKET;
+        }
+
+        sockaddress(int if_index, int protocol = ETH_P_ALL)
+        : _len(sizeof(sockaddr_ll))
+        {
+            memset(&_addr, 0, sizeof(sockaddr_ll));
+            _addr.sll_family   = AF_PACKET;
+            _addr.sll_ifindex  = if_index;
+            _addr.sll_protocol = htons(protocol); 
+        }
+
+
+        sockaddress(const std::string& if_name, int protocol = ETH_P_ALL)
+        : _len(sizeof(sockaddr_ll))
+        {
+            memset(&_addr, 0, sizeof(sockaddr_ll));
+            _addr.sll_family   = AF_PACKET;
+            _addr.sll_protocol = htons(protocol);
+            if ( ( _addr.sll_ifindex = if_nametoindex(if_name.c_str())) == 0 ) 
+            {
+                throw std::system_error(errno, std::system_category(), "Unkown Interface");
+            } 
+        }
+
+        sockaddress(const sockaddress&)            = default;
+        sockaddress& operator=(const sockaddress&) = default;
+        sockaddress(sockaddress&&)                 = default;
+        sockaddress& operator=(sockaddress&&)      = default;
+        ~sockaddress()                             = default;
+
+        int 
+        ifindex() const
+        {
+            return _addr.sll_ifindex;
+        }
+
+        std::string
+        ifname() const
+        {
+            char name[64];
+
+            if ( if_indextoname(_addr.sll_ifindex, name) == nullptr )
+            {
+                throw std::system_error(errno, std::system_category(), "Interface name error");
+            } 
+
+            return _addr.sll_ifindex == 0  ? "Any" : name; 
+        }
+
+
+        std::string
+        hw_addr() const{
+            std::stringstream ss;
+            for (auto i = 0 ; i <= 4; ++i)
+            {
+               ss << std::hex << static_cast<u_short>(_addr.sll_addr[i]) << ":";
+            }
+            ss << static_cast<u_short>(_addr.sll_addr[5]);
+            return ss.str();
+        }
+
+        unsigned short
+        hw_len() const{
+            return _addr.sll_halen;
+        }
+
+        unsigned short
+        hw_type() const
+        {
+            return _addr.sll_hatype;
+        }
+
+        unsigned short
+        pkt_type() const
+        {
+            return _addr.sll_pkttype;
+        }
+
+        unsigned short
+        family() const
+        {
+            return _addr.sll_family;
+        }
+
+
+        socklen_t
+        len() const{
+            return _len;
+        }
+
+        socklen_t &
+        len()
+        {
+            // return static_cast<socklen_t&>(_len);
+            return _len;
+        }
+
+        
+        const sockaddr&
+        c_addr() const
+        {
+            return reinterpret_cast<const sockaddr&>(_addr);
+        }
+        sockaddr&
+        
+        c_addr()
+        {
+            return reinterpret_cast<sockaddr&>(_addr);
+        }
+
+
+
+    };
+    #endif
 
 
 }
