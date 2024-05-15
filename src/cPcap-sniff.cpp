d9b8f2b4f1ef1dab785f6d09e7443e92868d61a1
“Gregorio <gregorio.procissi@unipi.it>
2024-05-15 10:46:42 +0200

    minor

--- src/cPcap-sniff.cpp
+++ src/cPcap-sniff.cpp
@@ -1,5 +1,6 @@
 #include <pcap/pcap.h>
 #include <iostream>
+#include <string>
 #include "packet.hpp"
 
 void elabora(u_char *user, const struct pcap_pkthdr *h,  const u_char *bytes)
@@ -8,8 +9,9 @@ void elabora(u_char *user, const struct pcap_pkthdr *h,  const u_char *bytes)
     auto cnt = reinterpret_cast<int*>(user);
     (*cnt)++;
     npl::packet<hdr::ether> pkt(bytes,h->caplen);
-    auto all = pkt.dump();
-    for (auto x : all) std::cout << PROTOCOL_NAME.at(x.first) << " : " ; 
+//    auto all_hdr = pkt.dump();
+//    for (auto x : all) std::cout << PROTOCOL_NAME.at(x.first) << " : " ; 
+    pkt.display();
     std::cout << std::endl;
 }
 
@@ -17,19 +19,35 @@ int main(int argc, char* argv[])
 {
  	pcap_t *handle;
  	char errbuf[PCAP_ERRBUF_SIZE];
-    const int SNAPLEN = 64;
- 
-   	// handle = pcap_open_live(argv[1], SNAPLEN, 1, 1000, errbuf);
-   	handle = pcap_open_offline(argv[1], errbuf);
+  const int SNAPLEN = 64;
+
+  if (argc != 3)
+  {
+      std::cout << "Usage: " << argv[0] << " -i <iface> | -f <filename>" << std::endl;
+      return EXIT_FAILURE;
+  }  
+
+  std::string flag(argv[1]);
+  std::string dev(argv[2]);
+
+  if (flag == "-f") {
+ 	    handle = pcap_open_offline(dev.c_str(), errbuf);
+      if (handle == NULL) {
+ 		  std::cerr <<  "Couldn't open file:  " << errbuf << std::endl;
+ 		  return(2);
+      }
+ 	} else {
+   	handle = pcap_open_live(dev.c_str(), SNAPLEN, 1, 1000, errbuf);
    	if (handle == NULL) {
    		std::cerr <<  "Couldn't open device:  " << errbuf << std::endl;
    		return(2);
-   	}
+    }
+ 	}
 
-    int state = 0;
-    pcap_loop(handle, -1, elabora, reinterpret_cast<u_char*>(&state));
+  int state = 0;
+  pcap_loop(handle, -1, elabora, reinterpret_cast<u_char*>(&state));
 
-    std::cout << "Number of processed packets: " << state << std::endl;
+  std::cout << "Number of processed packets: " << state << std::endl;
 
 //    for (;;)
 //    {
