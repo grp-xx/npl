02759290bb266a64e26db8884527d2f64c57019e
“Gregorio <gregorio.procissi@unipi.it>
2025-05-13 23:16:44 +0200

    using next and for_each

--- pcap-sniffer/src/main.rs
+++ pcap-sniffer/src/main.rs
@@ -1,29 +1,51 @@
-use pcap::{Capture, Device};
-
+use pcap::{BpfProgram, Capture, Device};
 
 fn main() {
     let interfaces = Device::list().unwrap();
     for i in interfaces.iter() {
-        println!("{}",i.name);
+        println!("{}", i.name);
     }
 
-//    let mydev = "en8";
-//    let mut cap = Capture::from_device(mydev)
-//    .expect("Can't open device")
-//    .immediate_mode(true)
-//    .promisc(true)
-//    .snaplen(64)
-//    .open()
-//    .expect("Can't open Pcap device");
+    let mydev = "en8";
+    let mut cap = Capture::from_device(mydev)
+        .expect("Can't open device")
+        .immediate_mode(true)
+        .promisc(true)
+        .snaplen(64)
+        .open()
+        .expect("Can't open Pcap device");
 
-    let mut cap = Capture::from_file("../Traces/smallFlows.pcap")
-    .expect("Can't open pacp file");
+    cap.filter("ip", true).unwrap();
+    //    let mut cap = Capture::from_file("../Traces/smallFlows.pcap").expect("Can't open pacp file");
+    //    println!("LinkType: {:?}", cap.get_datalink());
+    //    while let Ok(frame) = cap.next_packet() {
+    //        //     println!("Header {:?}",&pkt.header);
+    //        let ethertype = &frame.data[12..14];
+    //        if ethertype == [0x08, 0x00] {
+    //            println!("Packet: {:?}", &frame.data[14..]);
+    //            println!("Src Addr: {:?}", &frame.data[26..30]);
+    //            println!("Dst Addr: {:?}", &frame.data[30..34]);
+    //        }
+    //    }
+    //
 
-    while let Ok(pkt) = cap.next_packet() {
-//         println!("Header {:?}",pkt.header);
-        let ethertype = &pkt.data[12..14];
-        if ethertype == [0x08,0x00] {
-            println!("Packet: {:0x?}",&pkt.data[14..]);
+    let pkt_handler = |frame: pcap::Packet<'_>| {
+        let ethertype = &frame.data[12..14];
+        if ethertype == [0x08, 0x00] {
+            //     println!("Packet: {:?}", &frame.data[..]);
+            println!("_______________________");
+            println!(
+                "Timestamp: {}.{}",
+                frame.header.ts.tv_sec, frame.header.ts.tv_usec
+            );
+            println!("Src Addr: {:?}", &frame.data[26..30]);
+            println!("Dst Addr: {:?}", &frame.data[30..34]);
         }
-    }
+    };
+
+    // Mediante callback
+    //
+    // cap.for_each(Some(10), pkt_handler).unwrap();
+    cap.for_each(Some(30), pkt_handler).unwrap();
+    println!("Stats: {:?}", cap.stats().unwrap());
 }
