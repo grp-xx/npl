aa508e06f53dc8c7fc6ec653ac3ba7fd5d9c2a35
“Gregorio <gregorio.procissi@unipi.it>
2025-05-22 11:13:42 +0200

    added multithead capture

--- pnet_sniffer/src/main.rs
+++ pnet_sniffer/src/main.rs
@@ -1,5 +1,5 @@
 use pnet::datalink::{self, Channel::Ethernet};
-use pnet::datalink::{Config, EtherType, NetworkInterface};
+use pnet::datalink::{Config, FanoutOption, FanoutType, NetworkInterface};
 use pnet::packet::Packet;
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
 use pnet::packet::ipv4::Ipv4Packet;
@@ -11,41 +11,63 @@ fn set_interface(iface: &str) -> Option<NetworkInterface> {
 fn main() {
     let ifaces: Vec<_> = std::env::args().skip(1).collect();
     // let devices = datalink::interfaces();
+    let iface = &ifaces[0];
 
-    let interface = match set_interface(&ifaces[0]) {
-        Some(interface) => interface,
-        None => panic!("Device {} not found", &ifaces[0]),
-    };
+    let num_threads = 4;
+    let mut t = vec![];
 
-    let configuration = Config::default();
+    for i in 0..num_threads {
+        let iface = iface.clone();
+        t.push(std::thread::spawn(move || {
+            let interface = match set_interface(&iface) {
+                Some(interface) => interface,
+                None => panic!("Device {} not found", iface),
+            };
 
-    // println!("Default Configuration: {:?}", configuration);
+            let mut configuration = Config::default();
+            let f_out: FanoutOption = FanoutOption {
+                group_id: 1234,
+                fanout_type: FanoutType::HASH,
+                defrag: false,
+                rollover: true,
+            };
 
-    let (_tx, mut rx) = match datalink::channel(&interface, configuration) {
-        Ok(Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unsupported channel"),
-        Err(e) => panic!("{}", e),
-    };
+            configuration.linux_fanout = Some(f_out);
 
-    loop {
-        match rx.next() {
-            Ok(frame) => match EthernetPacket::new(frame) {
-                Some(eth_hdr) => match eth_hdr.get_ethertype() {
-                    EtherTypes::Ipv4 => {
-                        let ip = Ipv4Packet::new(eth_hdr.payload());
-                        if let Some(ip_hdr) = ip {
-                            println!("{:?}", ip_hdr);
-                        };
-                    }
-                    _ => {}
-                },
-                None => {}
-            },
-            Err(_) => {}
-        }
+            // println!("Default Configuration: {:?}", configuration);
+
+            let (_tx, mut rx) = match datalink::channel(&interface, configuration) {
+                Ok(Ethernet(tx, rx)) => (tx, rx),
+                Ok(_) => panic!("Unsupported channel"),
+                Err(e) => panic!("{}", e),
+            };
+
+            loop {
+                match rx.next() {
+                    Ok(frame) => match EthernetPacket::new(frame) {
+                        Some(eth_hdr) => match eth_hdr.get_ethertype() {
+                            EtherTypes::Ipv4 => {
+                                let ip = Ipv4Packet::new(eth_hdr.payload());
+                                if let Some(ip_hdr) = ip {
+                                    println!(
+                                        "Thread {}: {} --> {}",
+                                        i,
+                                        ip_hdr.get_source(),
+                                        ip_hdr.get_destination()
+                                    );
+                                };
+                            }
+                            _ => {}
+                        },
+                        None => {}
+                    },
+                    Err(_) => {}
+                }
+            }
+        }));
     }
 
-    //    for i in devices.iter() {
-    //        println!("{}",i.name);
-    //    }
+    for i in t {
+        let _ = i.join();
+    }
 }
