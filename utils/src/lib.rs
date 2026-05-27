54455ff32fd8e40f9747f94bca18adae71f34671
Gregorio Procissi <gregorio.procissi@unipi.it>
2026-05-27 16:37:52 +0200

    added net utils

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,5 +1,8 @@
 #[cfg(feature = "log")]
 pub mod log;
 
+#[cfg(feature = "net")]
+pub mod net;
+
 #[cfg(feature = "pnet-utils")]
 pub mod pnet;
---
+++ utils/src/net.rs
@@ -0,0 +1,25 @@
+use std::net::SocketAddrV4;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct FlowIdV4(pub SocketAddrV4, pub SocketAddrV4, pub u8);
+
+impl FlowIdV4 {
+    pub fn new(src: &SocketAddrV4, dst: &SocketAddrV4, proto: u8) -> Self {
+        if src <= dst {
+            Self(*src, *dst, proto)
+        } else {   
+        Self(*dst, *src, proto)
+        }
+    }
+}
+
+impl std::fmt::Display for FlowIdV4 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} -> {} (proto {})",
+            self.0,
+            self.1,
+            self.2
+        )
+    }
+}
