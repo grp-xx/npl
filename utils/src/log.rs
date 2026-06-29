9af83321d7a71ede7be2e8345f44e195f169b460
“Gregorio <gregorio.procissi@unipi.it>
2026-06-29 09:29:14 +0200

    updated utils

--- utils/src/log.rs
+++ utils/src/log.rs
@@ -3,3 +3,7 @@ pub fn verbose_log(verbose: u8, level: u8, message: String) {
         eprintln!("{message}");
     }
 }
+
+pub fn bold(s: &str) -> String {
+    format!("\x1b[1m{}\x1b[0m", s)
+}
--- utils/src/net.rs
+++ utils/src/net.rs
@@ -1,5 +1,6 @@
 use std::net::SocketAddrV4;
 use std::net::{IpAddr, ToSocketAddrs};
+use std::io::Result;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct FlowIdV4(pub SocketAddrV4, pub SocketAddrV4, pub u8);
@@ -47,4 +48,18 @@ pub fn hostname_to_ipv4(hostname: &str) -> std::io::Result<Vec<std::net::Ipv4Add
         .collect();
 
     Ok(ipv4_addrs)
+}
+
+pub fn get_ipv4_addr_from_udp_socket(socket: &std::net::UdpSocket) -> Result<String> {
+    let ip = socket.local_addr()?.ip();
+    let port = match socket.local_addr()?.port() {
+        50000 => 51000,
+        _ => 50000,
+    };
+    let addr = std::net::SocketAddr::new(ip, port);
+    let sock = std::net::UdpSocket::bind(addr)?;
+    let remote = "8.8.8.8:53";
+    sock.connect(remote)?;
+    let local =     sock.local_addr()?;
+    Ok(local.ip().to_string())
 }
